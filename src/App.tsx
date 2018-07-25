6acd76d454266c2ed2ba0ca92a365acd4a6678c1
Alicia Kacharia <t-alkac@microsoft.com>
2018-07-24 20:39:16 -0700

    footer stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import './App.css';
 import { Home } from './static/pages/Home';
-import { Footer, Header } from './static/shared'
+import { Header } from './static/shared'
+import Footer from './static/shared/Footer'; 
 
 class App extends React.Component {
   public render() {
