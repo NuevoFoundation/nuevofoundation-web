ef90ea42c6ba2b50e978ec504ab1f6da35f5919a
adrianaixba <ixbaadriana@gmail.com>
2018-09-20 23:40:40 -0500

    added more fields, having packaging issues

--- src/static/pages/Contact.tsx
+++ src/static/pages/Contact.tsx
@@ -7,21 +7,32 @@ const Title = styled.div`
   font-size:34px;
   font-weight:bold;
   padding-top:40px;
+  padding-bottom:30px;
 `
 
 interface IContactState {
+    email: string;
+    message: string;
     name: string;
+    subject: string;
 }
 export class Contact extends React.Component<{}, IContactState> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            name: ''
-        }
+            email: '',
+            message: '',
+            name: '',
+            subject: ''
+        };
     }
 
     public handleChange = (e: any) => {
-        this.setState({ name: e.target.value });
+         this.setState({ name: e.target.value });
+        // const change = {}
+        // change[e.target.name] = e.target.value
+        // this.setState(change)
+        //this.setState({ [e.target.type]: e.target.value });
     }
 
     public render() {
@@ -41,8 +52,32 @@ export class Contact extends React.Component<{}, IContactState> {
                                 <ControlLabel>Name</ControlLabel>
                                 <FormControl
                                     type="text"
-                                    onChange={this.handleChange}
+                                    name="name"
+                                    placeholder="Enter name"
+                                    onChange= {this.handleChange}
                                     value={this.state.name}
+                                />
+                                <ControlLabel>Email</ControlLabel>
+                                <FormControl
+                                    type="email"
+                                    name="email"
+                                    placeholder="Enter valid email"
+                                    onChange={this.handleChange}
+                                    value={this.state.email}
+                                />
+                                <ControlLabel>Subject</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="What are you interested in?"
+                                    onChange={this.handleChange}
+                                    value={this.state.subject}
+                                />
+                                <ControlLabel>Message</ControlLabel>
+                                <FormControl
+                                    type="textarea"
+                                    placeholder="Enter message"
+                                    onChange={this.handleChange}
+                                    value={this.state.message}
                                 />
                             </FormGroup>
                         </form>
