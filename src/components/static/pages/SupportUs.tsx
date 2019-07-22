da5e317cf3439ffe52fb64cdf4edfbbc1a619d02
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-07-22 14:45:27 -0700

    Added TODO Comments

--- src/components/static/pages/SupportUs.tsx
+++ src/components/static/pages/SupportUs.tsx
@@ -10,14 +10,14 @@ export const SupportUs: React.FC = (): JSX.Element => {
 
   return (
     <SupportUsPageWrapper>
-      <Paper style={{borderRadius: '0px', marginBottom: '16px', height: '320px'}}>
-        <Box style={{height: '100%', width: '25%'}}>
+      <Paper style={{borderRadius: '0px', marginBottom: '16px', height: '320px'}}> {/* TODO: Remove hardcoded dimensions when content is added */}
+        <Box style={{height: '100%', width: '25%'}}> {/* TODO: Remove hardcoded dimensions when content is added */}
           <Typography variant="h2" gutterBottom>
             Support Us
           </Typography>
         </Box>
       </Paper>
-      <div style={{height: `${window.innerHeight-320}px`}}>
+      <div style={{height: `${window.innerHeight-320}px`}}> {/* TODO: Remove hardcoded dimensions when content is added */}
         <Typography variant="h2">
             [TBD]
         </Typography>
