280805ba312542b9462c1d4492e65bb573ba9994
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-07-22 14:40:54 -0700

    Updated SupportUs page layout

--- src/components/static/pages/SupportUs.tsx
+++ src/components/static/pages/SupportUs.tsx
@@ -2,51 +2,26 @@ import * as React from "react";
 import styled from "styled-components";
 import "../../../assets/stylesheets/SupportUs.css";
 import { DonateButton } from "../common/DonateButton";
+import { Paper, Box, Typography } from '@material-ui/core'
 
 const SupportUsPageWrapper = styled.div``;
-const QuoteWrapper = styled.div`
-  padding-top: 300px;
-  background-color: #d2d2d2;
-  height: 430px;
-`;
 
-const SupportStatement = styled.div`
-  padding-top: 75px;
-  padding-bottom: 100px;
-`;
+export const SupportUs: React.FC = (): JSX.Element => {
 
-const DonateWrapper = styled.div``;
-
-export class SupportUs extends React.Component {
-  public render() {
-    return (
-      <SupportUsPageWrapper>
-        <QuoteWrapper>
-          <div className="quote">
-            "Games shouldn't only be fun. They should <br />
-            teach or spark an interest in other things."
-          </div>
-          <div className="author"> Hideo Kojima </div>
-        </QuoteWrapper>
-        <SupportStatement>
-          <div className="supportTitle">Support Nuevo Foundation</div>
-          <div className="missionStatement">
-            Nuevo Foundation mission is to change the way the world tackles
-            education by investing in companies,
-            <br />
-            leaders and ideas. Your donation will help build more businesses
-            that serve the poor. Simply complete the
-            <br />
-            form below to donate online.
-          </div>
-        </SupportStatement>
-        <DonateWrapper>
-          <div className="donate">Select an amount </div>
-          <DonateButton text="$20" /> <DonateButton text="$50" />{" "}
-          <DonateButton text="$100" /> <DonateButton text="$250" />{" "}
-          <DonateButton text="Other" />
-        </DonateWrapper>
-      </SupportUsPageWrapper>
-    );
-  }
+  return (
+    <SupportUsPageWrapper>
+      <Paper style={{borderRadius: '0px', marginBottom: '16px', height: '320px'}}>
+        <Box style={{height: '100%', width: '25%'}}>
+          <Typography variant="h2" gutterBottom>
+            Support Us
+          </Typography>
+        </Box>
+      </Paper>
+      <div style={{height: `${window.innerHeight-320}px`}}>
+        <Typography variant="h2">
+            [TBD]
+        </Typography>
+      </div>
+    </SupportUsPageWrapper>
+  )
 }
