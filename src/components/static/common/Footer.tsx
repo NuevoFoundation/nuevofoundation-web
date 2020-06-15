eb511a12c2e6bf4f6e697a1761f91dce2f6a5362
Ximena <ximena.cortez@kennasecurity.com>
2020-06-15 18:41:36 -0500

    updating youtube url

--- src/components/static/common/Footer.tsx
+++ src/components/static/common/Footer.tsx
@@ -58,7 +58,7 @@ export class Footer extends React.Component {
                   <a
                       target="_blank"
                       rel="noopener noreferrer"
-                      href="https://www.youtube.com/channel/UCsVAtli7SAqVGZFR5w-whBw"
+                      href="https://www.youtube.com/channel/UCsVAtli7SAqVGZFR5w-whBw?sub_confirmation=1"
                       className={"footer-pics"}
                   >
                     <FontAwesomeIcon icon={faYoutube} className={"fa-2x"} />
