f39db9ee4df221bb3ff3543c6a6ff7215984b65f
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2020-07-27 23:34:38 -0700

    Updating student and teach image paths
    Updating student and teach image paths

--- src/components/static/pages/GetInvolved.tsx
+++ src/components/static/pages/GetInvolved.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
-import teacher from "../../../assets/images/getinvolved/teacher.png";
-import student from "../../../assets/images/getinvolved/student.png";
+import teacher from "../../../assets/images/getinvolved/teachers.png";
+import student from "../../../assets/images/getinvolved/students.png";
 import volunteer from "../../../assets/images/getinvolved/volunteers.png";
 import { PageTitle } from "../common/PageTitle";
 import {
