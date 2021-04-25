5c9e2a48b865ea7368795274eb235def67ec0398
Ximena <ximena.cortez@kennasecurity.com>
2021-04-24 23:08:09 -0500

    new student numbers

--- src/components/static/pages/Home.tsx
+++ src/components/static/pages/Home.tsx
@@ -282,7 +282,7 @@ export class Home extends React.Component<{}, StatProps> {
     {
       title: "students",
       titleBoldened: "Diverse",
-      stat: "8,891",
+      stat: "9,284",
       description: "This means inclusively reflecting diversity of gender, ethnicity, age, and ability in our foundation, volunteers, speakers and workshop leaders."
     },
     {
