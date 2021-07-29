f311252cc95e3077030560a9185b17927fad910f
Ximena <ximena.cortez@kennasecurity.com>
2021-07-29 10:38:51 -0500

    new numbers

--- src/components/static/pages/Home.tsx
+++ src/components/static/pages/Home.tsx
@@ -282,7 +282,7 @@ export class Home extends React.Component<{}, StatProps> {
     {
       title: "students",
       titleBoldened: "Diverse",
-      stat: "9,418",
+      stat: "9,698",
       description: "This means inclusively reflecting diversity of gender, ethnicity, age, and ability in our foundation, volunteers, speakers and workshop leaders."
     },
     {
