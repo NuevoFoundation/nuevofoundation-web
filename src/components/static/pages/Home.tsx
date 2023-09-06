383a7f65297300bc3cab34adc7c1ecf05e1189a8
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2023-09-06 14:12:20 -0700

    Updating the homepage with new numbers stats
    Updating the homepage with new numbers stats

--- src/components/static/pages/Home.tsx
+++ src/components/static/pages/Home.tsx
@@ -284,7 +284,7 @@ export class Home extends React.Component<{}, StatProps> {
     {
       title: "students",
       titleBoldened: "Diverse",
-      stat: "14,948",
+      stat: "15,734",
       description: "This means inclusively reflecting diversity of gender, ethnicity, age, and ability in our foundation, volunteers, speakers and workshop leaders."
     },
     {
