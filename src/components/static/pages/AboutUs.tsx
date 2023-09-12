94b8721f0cbfc7848e3379983e3f7a553028b8b8
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2023-09-11 19:36:25 -0700

    Update AboutUs.tsx with grammar fixes.
    Update AboutUs.tsx with grammar fixes.

--- src/components/static/pages/AboutUs.tsx
+++ src/components/static/pages/AboutUs.tsx
@@ -86,7 +86,7 @@ export class AboutUs extends React.Component {
       {
         name: "Anvesha Sinha",
         role: "Curriculum Ninja",
-        quote: "-\"Be the change that you wish to see in the world\" -Mahatma Gandhi",
+        quote: "-\"Be the change that you wish to see in the world\" - Mahatma Gandhi",
         bio: "Anvesha joined Nuevo Foundation with the intention of inspiring and coaching students to pursue a career in STEM. She helps out with Nuevo's workshops experience, focusing mainly on coding. A proud holder of master's in computer science (CS), she tries to make complex concepts fun and interesting for beginners. She is passionate about educating kids around the globe about careers in computer science and giving them a fair chance in choosing the best career path. In her free time, she like going on hikes, reading & solving puzzles.",
         img:"anvesha",
         cartoon: "anvesha-cartoon"
@@ -102,7 +102,7 @@ export class AboutUs extends React.Component {
       {
         name: "Paola Ramirez",
         role: "Design Alchemist and Pixel Perfectionist",
-        quote: "-\"You don't have to be great to start, but you have to start to be great.\"",
+        quote: "\"You don't have to be great to start, but you have to start to be great.\" - Zig Ziglar",
         bio: "Paola is a Visual Designer from Puerto Rico with a bachelor’s degree in digital graphic design. Passionate about everything design and how it connects us to the world, she joined the Nuevo Foundation to help with their mission through design. In her free time, she likes to play video games and read a good book on a rainy day.",
         img: "paola",
         cartoon: "paola-cartoon"
@@ -134,7 +134,7 @@ export class AboutUs extends React.Component {
       {	
         name: "Kathryn Baldauf",	
         role: "Curriculum Genius",	
-        quote: "-\"Let us remember: One book, one pen, one child, and one teacher can change the world.\" -Malala Yousafzai",	
+        quote: "-\"Let us remember: One book, one pen, one child, and one teacher can change the world.\" - Malala Yousafzai",	
         bio: "Kathryn started at Microsoft as a new graduate in the summer of 2019. She works on Microsoft's open source Container Platform team as a software engineer. She's passionate about spreading education and positive change in the world. In her free time she enjoys hydroponic gardening, drinking tea, reading about a variety of topics, listening to music, and spending time with her friends",	
         img: "kathryn",	
         cartoon: "kathryn-cartoon"	
@@ -142,7 +142,7 @@ export class AboutUs extends React.Component {
       {	
         name: "Madeleyne Vaca",	
         role: "Hardware Lead",	
-        quote: "-\"Strive for significance, not success\"-Miller Templeton",
+        quote: "-\"Strive for significance, not success\"- Miller Templeton",
         bio: "Madeleyne got her engineering interest from her parents. Throughout grade school and college, she built robots and machines that fly, swim, drive, detect air quality, interpret sign language, and even survive in space. This was in large part due to the investment that the Georgia Tech College of Computing made in her early on in high school through their outreach programs. Recognizing this, Madeleyne began working for them, first as a high school assistant, eventually making her way up to leading her own workshops. Researching new topics to teach led her to the (day) job that she loves! Now, Nuevo Foundation gives her the opportunity to keep introducing kids to the wonderful world of hardware in hopes their lives can be changed for the better too.",	
         img: "madeleyne",	
         cartoon: "madeleyne-cartoon"	
@@ -150,7 +150,7 @@ export class AboutUs extends React.Component {
       {
         name: "Damaris Sardenberg",
         role: "Finance Officer",
-        quote: "\"A goal without a plan is just a wish\" -Antoine de Saint-Exupery",
+        quote: "\"A goal without a plan is just a wish\" - Antoine de Saint-Exupery",
         bio: "Damaris is passionate about helping others to discover their full potential leveraging Technology and Finance. She currently enables the digital transformation journey of Microsoft’s customers, partners & vendors in the Americas . She  is passionate for programs and organizations that support the protection and development of children and women from Latin America. She works closely with the Nuevo Foundation team providing pro-bono Financial support and she is also very active providing volunteering hours to increase diversity in the workplace.",
         img: "damaris",
         cartoon: "damaris-cartoon"
@@ -158,7 +158,7 @@ export class AboutUs extends React.Component {
       {
         name: "América Quistiano",
         role: "Community Lead",
-        quote: "-\"Your voice matters. What you do matters. So what are you going to do?\" -Anita Hill",
+        quote: "-\"Your voice matters. What you do matters. So what are you going to do?\" - Anita Hill",
         bio: "América started Microsoft as a new graduate during the fall of 2021 where she currently works for Bing Ads as a software engineer. América has had a passion for technology since she was little and that led her to a path in STEM which included studying computer science at UT Austin. She also found a huge passion for diversity & inclusion and helping others. This passion led her to working with the Nuevo Foundation during one of her summer internships at Microsoft and to becoming the president of the Hispanic Association of Computer Scientists at her university during her senior year. She enjoys working with the Nuevo Foundation because it allows her to share her unique experience with kids and to share her love for technology, with hopes of inspiring others to do great things.",
         img: "america",
         cartoon: "america-cartoon"
@@ -166,7 +166,7 @@ export class AboutUs extends React.Component {
       {
         name: "Joaquín Ruales",
         role: "Front-end Ninja",
-        quote: "-\"No matter what your ability is, effort is what ignites that ability and turns it into accomplishment.\" -Carol Dweck",
+        quote: "-\"No matter what your ability is, effort is what ignites that ability and turns it into accomplishment.\" - Carol Dweck",
         bio: "Joaquín is passionate about coding, explaining technical concepts simply, and designing user experiences to minimize user error. After having programmed web apps, robots, calculators, and mathematical art, he is now volunteering with Nuevo Foundation to participate in the redesign of their Workshops website. In his spare time, he enjoys playing speed chess and learning how to pronounce words in foreign languages.",
         img: "joaquin",
         cartoon: "joaquin-cartoon"
@@ -174,7 +174,7 @@ export class AboutUs extends React.Component {
       {
         name: "Eden Garza",
         role: "Curriculum Genius",
-        quote: "-\"Straight roads do not make skillful drivers.\"-Paula Coelho",
+        quote: "-\"Straight roads do not make skillful drivers.\"- Paula Coelho",
         bio: "Eden is a Software Engineer at Microsoft working on the Threat and Vulnerability Management team. Prior to that he studied Computer Science at The University of Texas at Austin with a focus on Systems and Security. He is passionate about volunteering with students, leading outreach efforts at UT and being a part of Nuevo since 2019. In his free time, he loves spending time with his cat, Mimi, going to the gym, and learning new languages.",
         img: "eden",
         cartoon: "eden-cartoon"
@@ -182,7 +182,7 @@ export class AboutUs extends React.Component {
       {
         name: "Lulu Espinoza",
         role: "Social Media Guru",
-        quote: "-\"Maybe it's time to believe that you can have everything you want.\" -Unknown",
+        quote: "-\"Maybe it's time to believe that you can have everything you want.\" - Unknown",
         bio: "Born in 1994, Guatemala, first child, and an art enthusiast. At 7, read a book called \"I want to be famous\" and could never stop reading literature since. Always been amazed by the aesthetic of surrealism and contemporary art. Graduated from International Affairs but worked as a journalist for a year, been a tutor/ teacher for over 8 years. Passion for working with kids, doing and learning many things at the same time! Believes in the power of education and how we use it to inspire others.",
         img: "lulu",
         cartoon: "lulu-cartoon"
@@ -198,7 +198,7 @@ export class AboutUs extends React.Component {
       {
         name: "Amira Najar",
         role: "Honorary Social Media Co-chair",
-        quote: "-\"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel\" -Maya Angelou",
+        quote: "-\"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel\" - Maya Angelou",
         bio: "Amira is a recent college grad and has been with Nuevo Foundation since December 2019. She is very passionate about giving back to her community and the power education has. She believes everyone deserves an equal opportunity in life, and hopes through her marketing efforts with NF, she is enabling the younger generations to believe in themselves and strive for their aspirations. Outside of being a product marketing manager, Amira enjoys working out, traveling all over the world and learning about better ways to self-improve.",
         img: "amira",
         cartoon: "amira-cartoon"
