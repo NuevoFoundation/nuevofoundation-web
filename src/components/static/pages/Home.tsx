e5ba99411821a65705bd81db1e5fd0d01585106f
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2023-10-17 23:14:05 -0700

    Update Home.tsx

--- src/components/static/pages/Home.tsx
+++ src/components/static/pages/Home.tsx
@@ -284,7 +284,7 @@ export class Home extends React.Component<{}, StatProps> {
     {
       title: "students",
       titleBoldened: "Diverse",
-      stat: "15,828",
+      stat: "15,903",
       description: "This means inclusively reflecting diversity of gender, ethnicity, age, and ability in our foundation, volunteers, speakers and workshop leaders."
     },
     {
