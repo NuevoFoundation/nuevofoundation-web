91a14bdb88e4ac4045fd2444bf0ec5cc0bdcd075
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2024-06-25 12:34:26 -0700

    Updated numbers! Yabba Dabba Doo!
    Updated numbers! Yabba Dabba Doo!

--- src/components/static/pages/Home.tsx
+++ src/components/static/pages/Home.tsx
@@ -284,7 +284,7 @@ export class Home extends React.Component<{}, StatProps> {
     {
       title: "students",
       titleBoldened: "Diverse",
-      stat: "18,047",
+      stat: "18,096",
       description: "This means inclusively reflecting diversity of gender, ethnicity, age, and ability in our foundation, volunteers, speakers and workshop leaders."
     },
     {
