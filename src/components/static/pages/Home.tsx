31b119072b4a510adda69013ab7ce445c9c3c6e1
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2024-12-11 02:38:59 -0800

    Updated positive impact student reach numbers!
    Updated positive impact student reach numbers!

--- src/components/static/pages/Home.tsx
+++ src/components/static/pages/Home.tsx
@@ -284,7 +284,7 @@ export class Home extends React.Component<{}, StatProps> {
     {
       title: "students",
       titleBoldened: "Diverse",
-      stat: "19,402",
+      stat: "19,584",
       description: "This means inclusively reflecting diversity of gender, ethnicity, age, and ability in our foundation, volunteers, speakers and workshop leaders."
     },
     {
