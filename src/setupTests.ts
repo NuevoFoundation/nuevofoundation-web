00ba69a09897d392a81e6ab6e0109bc56c69e058
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2025-05-22 19:33:11 -0700

    Revert to Node.js 20.x, add TextEncoder/TextDecoder polyfill, and update pipeline for compatibility

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,22 @@
+// Polyfill TextEncoder and TextDecoder for Node.js < 21
+import { TextEncoder as UtilTextEncoder, TextDecoder as UtilTextDecoder } from 'util';
+
+// Only polyfill if not already defined
+if (typeof globalThis.TextEncoder === 'undefined') {
+  Object.defineProperty(globalThis, 'TextEncoder', {
+    value: UtilTextEncoder,
+    configurable: true,
+    writable: true,
+  });
+}
+if (typeof globalThis.TextDecoder === 'undefined') {
+  Object.defineProperty(globalThis, 'TextDecoder', {
+    value: UtilTextDecoder,
+    configurable: true,
+    writable: true,
+  });
+}
+
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
