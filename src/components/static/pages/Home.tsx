edbaeaad23c5ed07eaf456570a3ac8655b7baa7c
Jeremiah Isaacson <jeremiahisaacson@hotmail.com>
2026-03-02 11:08:36 -0800

    Update student statistic in ImpactStats
    Update student statistic in ImpactStats

--- src/components/static/pages/Home.tsx
+++ src/components/static/pages/Home.tsx
@@ -266,7 +266,7 @@ export class Home extends React.Component<{}, StatProps> {
     {
       title: "students",
       titleBoldened: "Diverse",
-      stat: "22,334",
+      stat: "23,092",
       description: "This means inclusively reflecting diversity of gender, ethnicity, age, and ability in our foundation, volunteers, speakers and workshop leaders."
     },
     {
